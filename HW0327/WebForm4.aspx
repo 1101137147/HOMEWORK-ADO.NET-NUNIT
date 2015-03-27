b17e7cb0359d96dda6dcc1468e1412e8d34c0cfd
1101137147 <john830426000@yahoo.com.tw>
2015-03-27 22:37:57 +0800

    hw4.0

---
+++ HW0327/WebForm4.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="HW0327.WebForm4" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:TextBox ID="selecttext" runat="server"></asp:TextBox>
+        <asp:Button ID="select" runat="server" OnClick="btnselect_Click" Text="查詢" />
+    
+    </div>
+        <asp:GridView ID="GridView1" runat="server">
+            <Columns>
+             
+            </Columns>
+        </asp:GridView>
+        <asp:Label ID="namelabel" runat="server" Text="StudentName:"></asp:Label>
+        <asp:TextBox ID="name" runat="server"></asp:TextBox>
+        <asp:Label ID="agelabel" runat="server" Text="StudentAge:"></asp:Label>
+        <asp:TextBox ID="age" runat="server"></asp:TextBox>
+        <asp:Label ID="phonelabel" runat="server" Text="StudentPhone:"></asp:Label>
+        <asp:TextBox ID="phone" runat="server"></asp:TextBox>
+        <asp:Button ID="Insert" runat="server" OnClick="btnadd_Click" Text="新增" /></br>
+        <asp:Label ID="Label1" runat="server" Text="StudentId:"></asp:Label>
+        <asp:TextBox ID="sid" runat="server"></asp:TextBox>
+         <asp:Label ID="Label4" runat="server" Text="StudentName:"></asp:Label>
+        <asp:TextBox ID="name2" runat="server"></asp:TextBox>
+        <asp:Label ID="Label5" runat="server" Text="StudentAge:"></asp:Label>
+        <asp:TextBox ID="age2" runat="server"></asp:TextBox>
+        <asp:Label ID="Label6" runat="server" Text="StudentPhone:"></asp:Label>
+        <asp:TextBox ID="phone2" runat="server"></asp:TextBox>
+        <asp:Button ID="update" runat="server" OnClick="btnupdate_Click" Text="修改" />
+        <p>
+            StudentId:<asp:TextBox ID="Studentid" runat="server"></asp:TextBox>
+            <asp:Button ID="delete" runat="server"   OnClick="btndel_Click" Text="刪除" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ HW0327/WebForm4.aspx.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HW0327
+{
+    public partial class WebForm4 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            btnselect_Click(null, null);
+        }
+
+        protected void btnselect_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["HomeworkConnectionString"].ConnectionString))
+            {
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = "select * from Student where StudentName like @StudentName";
+                    cmd.Parameters.Add(new SqlParameter("@StudentName", "%" + selecttext.Text + "%"));
+
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        DataSet ds = new DataSet();
+                        da.Fill(ds);
+                        GridView1.DataSource = ds;
+                        GridView1.DataMember = "Table";
+                        GridView1.DataBind();
+                        da.Fill(ds);
+                    }
+                }
+            }
+        }
+        protected void btnadd_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["HomeworkConnectionString"].ConnectionString))
+            {
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = "select * from Student";
+                    cmd.Parameters.Add(new SqlParameter("@StudentName", "%" + selecttext.Text + "%"));
+
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        SqlCommandBuilder builder = new SqlCommandBuilder(da);
+                        DataSet ds = new DataSet();
+                        ds.Clear();
+                        da.Fill(ds);
+                        DataTable dt = ds.Tables["Table"];
+                        DataRow dr = dt.NewRow();
+                        dr["StudentName"] = name.Text;
+                        dr["StudentAge"] = age.Text;
+                        dr["StudentPhone"] = phone.Text;
+                        dt.Rows.Add(dr);
+                        da.Update(dt);
+
+                        btnselect_Click(null, null);
+                    }
+                }
+            }
+        }
+        protected void btnupdate_Click(object sender, EventArgs e)
+        {
+            string sql = @"update [Student] set [StudentName] = @StudentName, [StudentAge] = @StudentAge,[StudentPhone] = @StudentPhone
+                             where StudentId = @StudentId";
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["HomeworkConnectionString"].ConnectionString))
+            {
+                cn.Open();
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    cmd.Parameters.Add(new SqlParameter("@StudentId", sid.Text));
+                    cmd.Parameters.Add(new SqlParameter("@StudentName", name2.Text));
+                    cmd.Parameters.Add(new SqlParameter("@StudentAge", age2.Text));
+                    cmd.Parameters.Add(new SqlParameter("@StudentPhone", phone2.Text));
+                    cmd.ExecuteNonQuery();
+                }
+                btnselect_Click(null, null);
+            }
+        }
+
+        protected void btndel_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["HomeworkConnectionString"].ConnectionString))
+            {
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = "select * from Student";
+
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        SqlCommandBuilder builder = new SqlCommandBuilder(da);
+                        DataSet ds = new DataSet();
+                        ds.Clear();
+                        da.Fill(ds);
+                        DataTable dt = ds.Tables[0];
+                        DataRow dr = dt.Select(string.Format("StudentId= {0}", Studentid.Text)).First();
+                        dr.Delete();
+                        da.Update(dt);
+                        btnselect_Click(null, null);
+                    }
+                }
+            }
+        }
+
+    }
+
+
+}
