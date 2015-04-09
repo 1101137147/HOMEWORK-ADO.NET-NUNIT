43a3477d0d58114532e360dd0248a769b740e2b8
1101137147 <john830426000@yahoo.com.tw>
2015-04-09 15:17:59 +0800

    hw6.0

---
+++ Homework.test/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WebApplication1.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void CalYear()
+        {
+            Year.year cal = new Year.year();
+            int inputAge = 30;
+            int expect = 1988;
+
+            Assert.AreEqual(expect, cal.GetBirthYear(inputAge, Year.year.YearFormat.w));
+        }
+    }
+}
